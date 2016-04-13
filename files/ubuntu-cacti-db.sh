4215d99136332e1a491323a626aeaf8ea8f7f180
FBLGit <xmurias@pepino.fblnet.net>
2016-04-13 14:03:03 +0200

    F

--- files/ubuntu-cacti-db.sh
+++ files/ubuntu-cacti-db.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-if [ -v MYSQL_ROOT_PASSWORD ]; then
-   PWD=$MYSQL_ROOT_PASSWORD
-   USR=root
-fi
-if [ -v MYSQL_PASSWORD ]; then
-   PWD=$MYSQL_PASSWORD
-   USR=$MYSQL_USER
- fi
+PWD=$MYSQL_PASSWORD
+USR=$MYSQL_USER
+DB=$MYSQL_DATABASE
 
-echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO $USR@% IDENTIFIED BY $PWD;
+echo "GRANT ALL PRIVILEGES ON $DB.* TO $USR@% IDENTIFIED BY $PWD;
 FLUSH PRIVILEGES;">/data/sql_import.sql
 
 cp /data/sql_import.sql /docker-entrypoint-initdb.d/
