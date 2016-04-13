8dcf93820376a28eaead3e649f913e1e1d87cc04
FBLGit <xmurias@pepino.fblnet.net>
2016-04-13 14:12:01 +0200

    F

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ MAINTAINER FBLGIT
 VOLUME /var/lib/mysql
 COPY files/ubuntu-cacti-db.sh /data/ubuntu-cacti-db.sh
 RUN chmod +x /data/ubuntu-cacti-db.sh
-RUN /data/ubuntu-cacti-db.sh
+ONBUILD /data/ubuntu-cacti-db.sh
 
 EXPOSE 3306
 CMD ["mysqld"]
--- files/ubuntu-cacti-db.sh
+++ files/ubuntu-cacti-db.sh
@@ -4,7 +4,7 @@ PWD=$MYSQL_PASSWORD
 USR=$MYSQL_USER
 DB=$MYSQL_DATABASE
 
-echo "GRANT ALL PRIVILEGES ON $DB.* TO $USR@% IDENTIFIED BY $PWD;
+echo "GRANT ALL PRIVILEGES ON $DB.* TO $USR@% IDENTIFIED BY \'$PWD\';
 FLUSH PRIVILEGES;">/data/sql_import.sql
 
 cp /data/sql_import.sql /docker-entrypoint-initdb.d/
